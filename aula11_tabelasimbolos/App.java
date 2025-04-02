209ce3a8abaa001c257c7c3d503518680b9ce82c
leonardorodriguezherediapucrsonline2 <leonardorodriguezherediapucrsonline2@gmail.com>
2025-04-02 18:23:30 -0300

    Aula 11 - Tabela de Simbolos

---
+++ aula11_tabelasimbolos/App.java
@@ -0,0 +1,14 @@
+package aula11_tabelasimbolos;
+
+public class App {
+    public static void main(String[] args) {
+        TabelaSimbolo<String, String> carros = new TabelaSimbolo<>();
+        carros.atualizar("IBL-9444", "Tempra");
+        carros.atualizar("JDO-0025", "Vectra");
+        carros.atualizar("IZV-4444", "Tracker");
+        System.out.println(carros);
+
+        String c = "IZV-4443";
+        System.out.println(carros.buscar(c));
+    }
+}
---
+++ aula11_tabelasimbolos/TabelaSimbolo.java
@@ -0,0 +1,56 @@
+package aula11_tabelasimbolos;
+
+public class TabelaSimbolo<Chave, Valor> {
+    class Nodo<Chave, Valor> {
+        Chave chave;
+        Valor valor;
+        Nodo proximo;
+
+        public Nodo(Chave chave, Valor valor) {
+            this.chave = chave;
+            this.valor = valor;
+        }
+    }
+    private Nodo inicio;
+    private int tamanho;
+
+    public TabelaSimbolo() {
+        this.tamanho = 0;
+    }
+    public void atualizar(Chave chave, Valor valor) {
+        //atualizar("IBL-9444", "OPALA")
+        Nodo n = buscarNodo(chave);
+        if(n!=null) {
+            n.valor = valor;
+        }
+        else {
+            n = new Nodo(chave, valor);
+            n.proximo = inicio;
+            inicio = n;
+            tamanho++;
+        }
+    }
+    public Valor buscar(Chave chave) {
+        Nodo n = buscarNodo(chave);
+        if(n!=null) return (Valor) n.valor;
+        return null;
+    }
+    private Nodo buscarNodo(Chave chave) {
+        for (Nodo n = inicio; n!=null; n = n.proximo) {
+            if(n.chave.equals(chave)) return n;
+        }
+        return null;
+    }
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for(Nodo n = inicio; n!=null; n = n.proximo) {
+            sb.append(n.chave).append(" ").append(n.valor).append(System.lineSeparator());
+        }
+        return sb.toString();
+    }
+    public boolean contem(Chave chave) {
+        return buscarNodo(chave)!=null;
+    }
+}
+
