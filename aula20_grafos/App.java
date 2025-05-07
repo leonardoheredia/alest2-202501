b5ec936cf771d7177b8cf85c3d08d96a0851a11a
leonardorodriguezherediapucrsonline2 <leonardorodriguezherediapucrsonline2@gmail.com>
2025-05-07 20:33:54 -0300

    Aula 20 - Grafos

--- aula20_grafos/App.java
+++ aula20_grafos/App.java
@@ -9,5 +9,11 @@ public class App {
         System.out.println(g.gerarDot());
         System.out.println(g.obterGrau(0));
         System.out.println(g.adjacentes(0).toString());
+
+        Digrafo dg = new Digrafo(4);
+        dg.adicionarAresta(0,1);
+        dg.adicionarAresta(0,3);
+        dg.adicionarAresta(2,3);
+        System.out.println(dg.gerarDot());
     }
 }
--- aula20_grafos/Digrafo.java
+++ aula20_grafos/Digrafo.java
@@ -1,4 +1,29 @@
 package aula20_grafos;
 
 public class Digrafo {
+    private boolean[][] matriz;
+    private int nVertices;
+    private int nArestas;
+
+    public Digrafo(int nVertices) {
+        this.nVertices = nVertices;
+        matriz = new boolean[this.nVertices][this.nVertices];
+        nArestas = 0;
+    }
+    public void adicionarAresta(int v, int w) {
+        if(matriz[v][w]) return; //se ja existir cai fora
+        matriz[v][w] = true;
+        nArestas++;
+    }
+    public String gerarDot() {
+        StringBuilder sb = new StringBuilder("digraph { ").append(System.lineSeparator());
+        for (int v = 0; v < nVertices; v++) {
+            for (int w = 0; w < nVertices; w++) {
+                if(matriz[v][w]) sb.append(v).append("->").append(w).append(System.lineSeparator());
+            }
+        }
+        sb.append("}");
+        return sb.toString();
+    }
+
 }
