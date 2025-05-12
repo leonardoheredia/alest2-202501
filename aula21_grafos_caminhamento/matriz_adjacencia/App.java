ef6bf50c21650d21ae277ddfe8cfbea2c9a4b7ac
leonardorodriguezherediapucrsonline2 <leonardorodriguezherediapucrsonline2@gmail.com>
2025-05-12 20:12:16 -0300

    Aula 21 - Caminhamento

--- aula21_grafos_caminhamento/matriz_adjacencia/App.java
+++ aula21_grafos_caminhamento/matriz_adjacencia/App.java
@@ -4,5 +4,18 @@ import aula20_grafos.Grafo;
 
 public class App {
     public static void main(String[] args) {
+        Grafo g = new Grafo(5);
+        g.adicionarAresta(0,1);
+        g.adicionarAresta(1,2);
+        g.adicionarAresta(2,3);
+        g.adicionarAresta(2,4);
+        g.adicionarAresta(3,4);
+
+        BuscaEmProfundidade buscaOrigem4 = new BuscaEmProfundidade(g, 4);
+        buscaOrigem4.mostrarResultado();
+
+        System.out.println("Caminho do 4 para o 0");
+        buscaOrigem4.caminhoPara(0);
+
     }
 }
--- aula21_grafos_caminhamento/matriz_adjacencia/BuscaEmProfundidade.java
+++ aula21_grafos_caminhamento/matriz_adjacencia/BuscaEmProfundidade.java
@@ -6,6 +6,47 @@ import java.util.ArrayList;
 
 public class BuscaEmProfundidade {
     private Grafo grafo;
+    private int origem;
     private boolean[] percorridos;
     private int[] anteriores;
+
+    public BuscaEmProfundidade(Grafo g, int origem) {
+        this.grafo = g;
+        this.origem = origem;
+        percorridos = new boolean[g.getnVertices()];
+        anteriores = new int[g.getnVertices()];
+        for(int i = 0; i<anteriores.length; i++) anteriores[i] = -1;
+        buscarEmProfundidadeRecursivo(this.grafo, this.origem);
+    }
+
+    private void buscarEmProfundidadeRecursivo(Grafo g, int v) {
+        percorridos[v] = true;
+        ArrayList<Integer> adjacentesDoV = g.adjacentes(v);
+        for(Integer w:adjacentesDoV) {
+            if(!percorridos[w]) {
+                anteriores[w] = v;
+                buscarEmProfundidadeRecursivo(g, w);
+            }
+        }
+    }
+
+    public void caminhoPara(int destino) {
+        //mostrar o caminho do vertice origem para o destino
+        //lembrar que a origem esta em uma propriedade da classe
+
+    }
+
+    public void mostrarResultado() {
+        for (int v = 0; v < anteriores.length; v++) {
+            System.out.println(v + "  " + percorridos[v] + " " + anteriores[v]);
+        }
+    }
 }
+
+
+
+
+
+
+
+
