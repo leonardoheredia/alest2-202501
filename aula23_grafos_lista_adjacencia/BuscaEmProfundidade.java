02cb8c9b0bde0432def16325ec5abf39e871ac52
leonardorodriguezherediapucrsonline2 <leonardorodriguezherediapucrsonline2@gmail.com>
2025-05-19 20:14:34 -0300

    Aula 23 - Lista de Adjacencia

--- aula23_grafos_lista_adjacencia/BuscaEmProfundidade.java
+++ aula23_grafos_lista_adjacencia/BuscaEmProfundidade.java
@@ -1,4 +1,19 @@
 package aula23_grafos_lista_adjacencia;
 
 public class BuscaEmProfundidade {
+    private GrafoListaAdjacencia grafo;
+    private int origem;
+    private boolean[] percorridos;
+    private int[] anteriores;
+    public BuscaEmProfundidade(GrafoListaAdjacencia grafo, int origem) {
+        this.grafo = grafo;
+        this.origem = origem;
+        percorridos = new boolean[this.grafo.getNumVertices()];
+        anteriores = new int[this.grafo.getNumVertices()];
+        for(int v=0;v<this.grafo.getNumVertices();v++) anteriores[v] = -1;
+
+    }
+    private void buscarEmProfundidadeRecursivo(int v) {
+        //IMPLEMENTAR
+    }
 }
